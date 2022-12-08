89dc94fd050f16f098b4f019fd09d9e96b1f9fc3
Hiromitsu Iwata <hiromitsu7@gmail.com>
2022-12-08 20:59:14 +0900

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FieldComponent } from './field/field.component';
+import { LifeComponent } from './life/life.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FieldComponent,
+    LifeComponent
   ],
   imports: [
     BrowserModule
